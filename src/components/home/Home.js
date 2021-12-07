6fc662c4d824c370c43f53474ca5e4518081149c
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-12-08 01:25:14 +0600

    Initial commit

---
+++ src/components/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../Layout';
+
+const Home = () => {
+    return (
+        <Layout title="Home Page" className="container-fluid">
+            <h1>I am home page</h1>
+        </Layout>
+    );
+};
+
+export default Home;
