5a35be5b7ad8a9e5da79ccefd9c83aa1291e6663
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-12-08 02:06:51 +0600

    7. Sign Out User
    Video : 8

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, withRouter } from "react-router-dom";
+import { signout, isAuthenticated } from '../utils/auth';
 
 const isActive = (history, path) => {
     if (history.location.pathname === path) {
@@ -17,12 +18,23 @@ const Menu = ({ history }) => {
                 <li className="nav-item">
                     <Link className="nav-link" style={isActive(history, '/')} to="/">Home</Link>
                 </li>
-                <li className="nav-item">
-                    <Link className="nav-link" style={isActive(history, '/login')} to="/login">Login</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" style={isActive(history, '/register')} to="/register">Register</Link>
-                </li>
+                {!isAuthenticated() && (<>
+                    <li className="nav-item">
+                        <Link className="nav-link" style={isActive(history, '/login')} to="/login">Login</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" style={isActive(history, '/register')} to="/register">Register</Link>
+                    </li>
+                </>)}
+                {isAuthenticated() && (<>
+                    <li className="nav-item">
+                        <span className="nav-link" style={{ cursor: 'pointer', color: 'grey' }} onClick={() => {
+                            signout(() => {
+                                history.push('/login')
+                            });
+                        }} >Log Out</span>
+                    </li>
+                </>)}
             </ul>
         </nav>
     );
