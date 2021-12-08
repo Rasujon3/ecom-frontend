c0dcd04b135ec5ac6463977625b7744ccddd6df9
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-12-09 01:15:52 +0600

    filter product bt category part 2
    v-7

--- src/components/home/Checkbox.js
+++ src/components/home/Checkbox.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Checkbox = ({ categories }) => {
+    const [checked, setChecked] = useState([]);
+    const checkedIds = [...checked];
+
+    const handleToggle = id => () => {
+        // return -1 or first index
+        const foundId = checked.indexOf(id);
+        if (foundId === -1) {
+            checkedIds.push(id);
+        } else {
+            checkedIds.splice(foundId, 1);
+        }
+        setChecked(checkedIds);
+    }
+    useEffect(() => {
+        alert(JSON.stringify(checked));
+    }, [checked])
+
     return categories.map(category => (
         <li className="list-unstyled" key={category._id}>
             <input
+                onChange={handleToggle(category._id)}
+                value={checked.indexOf(category._id === -1)}
                 type="checkbox"
                 className="form-check-input"
             />
